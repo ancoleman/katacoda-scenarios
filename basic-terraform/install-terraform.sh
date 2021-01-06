377212f3614ead5cd98f63ce1095463187256431
Anton Coleman <acoleman@paloaltonetworks.com>
2021-01-06 11:11:40 -0600

    output to file

--- basic-terraform/install-terraform.sh
+++ basic-terraform/install-terraform.sh
@@ -4,4 +4,4 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && sudo apt-get install terraform
 tf_version=terraform --version
-echo "Finished installing Terraform version: $tf_version"
+echo "Finished installing Terraform version: $tf_version" >> /opt/.backgroundfinished
