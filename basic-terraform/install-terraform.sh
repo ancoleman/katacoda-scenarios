16effb399e5708401244532a0c66c57ab29d5e96
Anton Coleman <acoleman@paloaltonetworks.com>
2021-01-06 11:06:03 -0600

    update with background messages for environment setup

--- basic-terraform/install-terraform.sh
+++ basic-terraform/install-terraform.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "Installing Latest Terraform Version...Please Wait..."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && sudo apt-get install terraform
+tf_version=terraform --version
+echo "Finished installing Terraform version: $tf_version"
